feee20ad08ccbc0b61923ba24243f61c738b554a
Allie Robinson <allierob93@gmail.com>
2020-06-24 18:54:00 -0500

    added basic header

---
+++ src/components/list.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './list.css';
+
+const List = () => {
+    return (<div>
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>)
+}
---
+++ src/components/navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './navbar.css';
+
+const NavBar = () => {
+    return (<nav>
+        Weed List
+    </nav>)
+}
+
+export default NavBar
